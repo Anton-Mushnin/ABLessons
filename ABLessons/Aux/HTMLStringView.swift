334797b7b19a621fc018d14ef51aa758f0e4b3c9
Anton Mushnin <anton.mushnin@gmail.com>
2022-02-08 14:47:28 +0300

    Links open in Safari

--- ABLessons/Aux/HTMLStringView.swift
+++ ABLessons/Aux/HTMLStringView.swift
@@ -12,16 +12,44 @@ import SwiftUI
 
 
 struct HTMLStringView: UIViewRepresentable {
-    let htmlContent: String
-  
-    func makeUIView(context: Context) -> WKWebView {
-      let webView = WKWebView()
-      webView.scrollView.showsVerticalScrollIndicator = false
-      return webView
-    }
 
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        let htmlContentNotArial = htmlContent.replacingOccurrences(of: "font-family: Arial", with: "")
-        uiView.loadHTMLString(htmlContentNotArial, baseURL: nil)
+  let htmlContent: String
+
+  class Coordinator: NSObject, WKNavigationDelegate {
+      var parent: HTMLStringView
+
+      init(_ parent: HTMLStringView) {
+          self.parent = parent
+      }
+
+      // Delegate methods go here
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+      if navigationAction.navigationType == .linkActivated  {
+        if let url = navigationAction.request.url {
+          UIApplication.shared.open(url)
+          decisionHandler(.cancel)
+        }
+      } else {
+        decisionHandler(.allow)
+      }
     }
+  }
+  
+  
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+  
+  
+  func makeUIView(context: Context) -> WKWebView {
+    let webView = WKWebView()
+    webView.scrollView.showsVerticalScrollIndicator = false
+    return webView
+  }
+
+  func updateUIView(_ uiView: WKWebView, context: Context) {
+    uiView.navigationDelegate = context.coordinator
+    let htmlContentNotArial = htmlContent.replacingOccurrences(of: "font-family: Arial", with: "")
+    uiView.loadHTMLString(htmlContentNotArial, baseURL: nil)
+  }
 }
